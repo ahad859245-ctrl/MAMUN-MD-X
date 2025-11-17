57b24c9f2baad814ecbac1b968692372fc3d5bea
ahad859245-ctrl <ahad859245@gmail.com>
2025-11-18 00:44:34 +0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'MAMUN-MD-X',
+  author: '‎',
+  botName: "ₛₒᵣₖₐᵣᵢ ₐddₐ Gᵣₒᵤₚ💫",
+  botOwner: 'ᴍᴀᴍᴜɴ ᴀᴅᴍɪɴ', // Your name
+  ownerNumber: '8801786269298', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.4",
+  updateZipUrl: "https://github.com/ahad859245-ctrl/MAMUN-MD-X.git",
+};
+
+module.exports = settings;
